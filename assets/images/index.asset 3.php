61729f66b3be057c660ba4d214f7141527ef9973
Niklas Johansson <15849048+niklas-joh@users.noreply.github.com>
2026-03-29 07:41:09 +0200

    fix(lint): fix CSS indentation and quote style to pass stylelint

---
+++ assets/images/index.asset 3.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => 'add934fd0cbf8ab1f81d');
---
+++ assets/images/index.asset 5.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => 'dd445b40b9b7fd1bf921');
