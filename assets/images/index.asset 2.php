d6cd24da07ab0269173a5796a7b7755cbf88d158
Niklas Johansson <15849048+niklas-joh@users.noreply.github.com>
2026-03-29 07:41:39 +0200

    chore: remove duplicate asset files with spaces from build

--- assets/images/index.asset 2.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => 'add934fd0cbf8ab1f81d');
--- assets/images/index.asset 6.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => '914c97f8c826bfbb4a28');
