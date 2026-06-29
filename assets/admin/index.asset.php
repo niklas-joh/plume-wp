cdce22ada8def98144a49ceeeab76da25537fe6d
Niklas Johansson <15849048+niklas-joh@users.noreply.github.com>
2026-06-29 11:45:38 +0000

    fix(credits): rebuild assets for React credits display update
    Co-Authored-By: Niklas Johansson <15849048+niklas-joh@users.noreply.github.com>

--- assets/admin/index.asset.php
+++ assets/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'dbbbe96372d693fc1cff');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ed9e3fb7ab1e76fe64d4');
